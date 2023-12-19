54004334a48bffebe7452fe787abb79d022758dd
mehdi <mehdi.alouane@laplateforme.io>
2023-12-19 10:54:56 +0100

    ajout librairie Hashlib et mdp crypté

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+import hashlib
 
 def verifier_mot_de_passe(mot_de_passe):
     if len(mot_de_passe) < 8:
@@ -14,11 +15,16 @@ def verifier_mot_de_passe(mot_de_passe):
     else:
         return True
 
+def crypter_mot_de_passe(mot_de_passe):
+    return hashlib.sha256(mot_de_passe.encode()).hexdigest()
+
 def demander_mot_de_passe():
     while True:
         mot_de_passe = input("Veuillez choisir un mot de passe : ")
         if verifier_mot_de_passe(mot_de_passe):
-            print("Votre mot de passe est valide.")
+            mot_de_passe_crypte = crypter_mot_de_passe(mot_de_passe)
+            print("Votre mot de passe est valide et a été crypté avec succès.")
+            print("Mot de passe crypté : ", mot_de_passe_crypte)
             break
         else:
             print("Votre mot de passe n'est pas valide. Veuillez réessayer.")
